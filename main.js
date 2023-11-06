17df818ea92e71a2c49652614f87bceef2fc5b00
emanuele2377 <emanuelekushi7@gmail.com>
2023-11-06 22:59:21 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+ const numero = parseInt( prompt("Inserisci un numero") );
+ let risultato = pariOrDispari(numero);
+ console.log("Il tuo numero è " + risultato);
+
+ function pariOrDispari(numero) {
+    let risultato;
+
+    for (let i = 1; i < 6; i++) {
+        
+
+    }      
+    
+    function somma(numero, numero) {
+        
+        return (numero+numero);
+    }
+
+    if(numero % 2 == 0) {
+        risultato = "pari";
+    } else {
+        risultato = "dispari";
+    }
+
+    return risultato;
+}
+
